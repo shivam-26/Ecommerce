08334d2313c24808ca51bbffdb1165059c493ee5
shivam pandey <shivamvns111@gmail.com>
2023-02-16 20:19:45 +0530

    update in footer

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -17,14 +17,14 @@ const Footer = () => {
         <h1>ECOMMERCE.</h1>
         <p>High Quality is our first priority</p>
 
-        <p>Copyrights 2021 &copy; MeAbhiSingh</p>
+        <p>Copyrights 2023 &copy; Shivam Pandey</p>
       </div>
 
       <div className="rightFooter">
         <h4>Follow Us</h4>
-        <a href="http://instagram.com/meabhisingh">Instagram</a>
-        <a href="http://youtube.com/6packprogramemr">Youtube</a>
-        <a href="http://instagram.com/meabhisingh">Facebook</a>
+        <a href="http://instagram.com/shivampandey7518">Instagram</a>
+        <a href="https://www.linkedin.com/in/shivam-26/">Linkedin</a>
+        <a href="https://www.facebook.com/profile.php?id=100037691152160">Facebook</a>
       </div>
     </footer>
   );
